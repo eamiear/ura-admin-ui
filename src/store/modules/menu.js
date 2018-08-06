074f2d32e7d253cc5466e0e383e88d4619baff75
eamiear <2639121731@qq.com>
2018-08-06 16:55:34 +0800

    menu store

--- src/store/modules/menu.js
+++ src/store/modules/menu.js
@@ -1,12 +1,30 @@
+import { SET_MENU_NAV_LIST, SET_PERMISSIONS, UPDATE_MENU_NAV_ACTIVE_NAME } from '../mutation-types'
 const menu = {
   state: {
-
+    menuNavActiveName: '',
+    menuNavList: [],
+    permissions: []
   },
   mutations: {
-
+    [SET_MENU_NAV_LIST] (state, {menuNavList}) {
+      state.menuNavList = menuNavList
+    },
+    [SET_PERMISSIONS] (state, {permissions}) {
+      state.permissions = permissions
+    },
+    [UPDATE_MENU_NAV_ACTIVE_NAME] (state, {menuNavActiveName}) {
+      state.menuNavActiveName = menuNavActiveName
+    }
   },
   actions: {
+    generateSidebarMenus ({commit}) {
+      return new Promise(function(resolve, reject) {
 
+      });
+    },
+    updateMenuNavActiveName ({commit}, {activeName}) {
+      commit('UPDATE_MENU_NAV_ACTIVE_NAME', {activeName})
+    }
   }
 }
 
--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -2,8 +2,12 @@
 export const SWITCH_SIDEBAR_COLLAPSE = 'switch_sidebar_collapse'
 export const UPDATE_DOCUMENT_CLIENT_HEIGHT = 'update_document_client_height'
 
-
 // menu mutations
+export const SET_MENU_NAV_LIST = 'set_menu_nav_list'
+export const SET_PERMISSIONS = 'set_permissions'
+export const UPDATE_MENU_NAV_ACTIVE_NAME = 'upate_menus_nav_active_name'
+
+// tabs mutations
 export const UPDATE_TABS_ACTIVE_NAME = 'update_tabs_active_name'
 export const CLEAR_TABS_NAV_LIST = 'clear_tabs_nav_list'
 export const ADD_TABS_NAV_LIST = 'add_tabs_nav_list'
