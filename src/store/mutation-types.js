f9cb572a1adca503850c2586853b27183860e06d
eamiear <2639121731@qq.com>
2018-08-06 16:46:15 +0800

    mutations types

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,3 +1,22 @@
 // common mutation
 export const SWITCH_SIDEBAR_COLLAPSE = 'switch_sidebar_collapse'
 export const UPDATE_DOCUMENT_CLIENT_HEIGHT = 'update_document_client_height'
+
+
+// menu mutations
+export const UPDATE_TABS_ACTIVE_NAME = 'update_tabs_active_name'
+export const CLEAR_TABS_NAV_LIST = 'clear_tabs_nav_list'
+export const ADD_TABS_NAV_LIST = 'add_tabs_nav_list'
+export const DEL_TABS_NAV_LIST = 'del_tabs_nav_list'
+
+export const CLOSE_OTHER_TABS_NAV_LIST = 'close_other_tabs_nav_list'
+export const CLOSE_LEFT_TABS_NAV_LIST = 'close_left_tabs_nav_list'
+export const CLOSE_RIGHT_TABS_NAV_LIST = 'close_right_tabs_nav_list'
+
+// user mutations
+export const SET_TOKEN = 'set_token'
+export const SET_UID = 'set_uid'
+export const SET_INTRODUCTION = 'set_introduction'
+export const SET_NAME = 'set_name'
+export const SET_AVATAR = 'set_avatar'
+export const SET_USER_INFO = 'set_user_info'
